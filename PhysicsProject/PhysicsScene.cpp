4f325f87e9765ec4c05c165fb7087ea99a0f52ed
Britt <brittboudreaux@yahoo.com>
2021-08-19 12:57:31 -0500

    Collision Checks WIP, Added Collision Detection

--- PhysicsProject/PhysicsScene.cpp
+++ PhysicsProject/PhysicsScene.cpp
@@ -1,5 +1,6 @@
 #include "PhysicsScene.h"
 #include "PhysicsObject.h"
+#include "Sphere.h"
 
 PhysicsScene::PhysicsScene() : m_timeStep(0.01f), m_gravity(glm::vec2(0,0))
 {
@@ -33,6 +34,20 @@ void PhysicsScene::update(float deltatime)
 			actor->fixedUpdate(m_gravity, m_timeStep);
 		}
 		accumaltedtime -= m_timeStep;
+
+		for (auto outer = m_actors.begin(); outer != --m_actors.end(); outer++) {
+			for (auto inner = ++outer; inner != m_actors.end(); inner++) {
+
+				PhysicsObject* object1 = *outer;
+				PhysicsObject* object2 = *inner;
+				
+				//CollisionCheck
+				sphereToSphere(dynamic_cast<Sphere*>(object1), dynamic_cast<Sphere*>(object2));
+			}
+		
+				
+
+		}
 	}
 }
 
@@ -41,4 +56,9 @@ void PhysicsScene::draw()
 	for (PhysicsObject* actor : m_actors) {
 		actor->draw();
 	}
+}
+
+bool PhysicsScene::sphereToSphere(Sphere* Sphere1, Sphere* Sphere2)
+{
+	return false;
 }
